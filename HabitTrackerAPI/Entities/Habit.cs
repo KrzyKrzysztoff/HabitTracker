faccfbe113a0f564bc242ec18085066f4a62c0fd
Krzysiek Jaworski <krzysztof.jaworski98@gmail.com>
2023-11-02 06:33:20 +0100

    create main entities

---
+++ HabitTrackerAPI/Entities/Habit.cs
@@ -0,0 +1,22 @@
+﻿namespace HabitTrackerAPI.Entities
+{
+    internal class Habit
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdateDate { get; set; } = DateTime.Now;
+        public ImportantLevel ImportantLevel { get; set; }
+        public bool IsActive { get; set; }
+        public int ActiveDaysStrike{ get; set; }
+    }
+
+    enum ImportantLevel
+    {
+        None,
+        Low,
+        Medium,
+        Hight
+    }
+}
---
+++ HabitTrackerAPI/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace HabitTrackerAPI.Entities
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public string EmailAdress { get; set; }
+        public string Password { get; set; }
+        public List<Habit> Habits { get; set; }
+    }
+}
